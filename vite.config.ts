fad30be3766ee6af8f3c51ab6bb4ac7736ca966b
Vishnu Mouli <vishnumouli0@gmail.com>
2025-05-31 23:32:07 +0530

    added vite plugin node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react(), tailwindcss()],
+	plugins: [TanStackRouterVite({ target: "react", autoCodeSplitting: true }), react(), tailwindcss(), nodePolyfills()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
